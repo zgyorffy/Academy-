87cab68d7137c42c4b57ffbb9b56860568984056
Zsolt GYORFFY <zgyorffy@e1p4.cluj.42.fr>
2015-12-05 10:32:27 +0100

    modified get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zgyorffy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/05 08:53:15 by zgyorffy          #+#    #+#             */
-/*   Updated: 2015/12/05 10:02:17 by zgyorffy         ###   ########.fr       */
+/*   Updated: 2015/12/05 10:31:47 by zgyorffy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,23 @@
 static int	get_data (int fd, **cache)
 {
 	char	*buff;
-	ls -
+	char	*temp;
+	int		res;
+
+	buff = ft_strnew(BUFF_SIZE + 1);
+	if (!buff)
+		return(-1);
+	res = read(fd, buff, BUFF_SIZE);
+	if (res > 0)
+	{
+		buff[res] = '\0';
+		temp = ft_strjoin(*cache, buff);
+		free(*cache);
+		*cache = temp;
+	}
+	free(buff);
+	return (res);
+}
 
 int	get_next_line(int const fd, char **line)
 {
@@ -28,6 +44,7 @@ int	get_next_line(int const fd, char **line)
 		return (-1);
 	while ((eof = ft_strchr(cache, '\n')) == 0)
 	{
+
 	}
 	*line = ft_strsub(cache, 0, ft_strlen(cache) - ft_strlen(eof));
 	chache = ft_strdup(eof + 1);
